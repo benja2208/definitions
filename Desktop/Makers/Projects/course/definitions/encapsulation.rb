1772a0399f50a841bb893cb5e0be5a3bb2de0e91
Benjamina Bollag <benjamina.bollag12@imperial.ac.uk>
2015-07-29 17:07:16 +0100

    Delete encapsulation.rb

--- Desktop/Makers/Projects/course/definitions/encapsulation.rb
+++
@@ -1,4 +0,0 @@
-# definition: Encapsulation means that the internal representation 
-# of an object is hidden from the outside. In other words, it enables 
-# the user to modify certain aspects without knowing about the class.
-
